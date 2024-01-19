2be714143c2a7d612395691ab433ce9ea577c584
joaogabriel87 <joagabriel9@gmail.com>
2024-01-19 15:43:08 -0300

    tokenJWT

--- Program.cs
+++ Program.cs
@@ -1,6 +1,25 @@
 using Agendamento.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddAuthentication(
+    x =>
+    {
+        x.DefaultAuthenticateSheme = JwtBearerDefaults.AddAuthenticationSheme;
+        x.DefaultChallengeSheme = JwtBearerDefaults.AddAuthenticationSheme;
+    }
+);
+.AddJwtBearer(x =>
+{
+    x.RequireHttpsMetadata = false;
+    x.SaveToken = true;
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(Configuration.PrivateKey)),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
+builder.Services.AddAuthorization();
 builder.Services.AddTransient<TokenService>();
 
 // Add services to the container.
@@ -9,6 +28,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
