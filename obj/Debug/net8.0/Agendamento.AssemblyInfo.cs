91e7d8f369cd6060a878bfc163dd2a0f82826d02
joaogabriel87 <joagabriel9@gmail.com>
2024-01-31 23:50:02 -0300

    commit all

--- obj/Debug/net8.0/Agendamento.AssemblyInfo.cs
+++ obj/Debug/net8.0/Agendamento.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Agendamento")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f88fe855e736757089480908023f683fbb65e752")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7f3a59c6ae12b008bd9a37871c8f91ffeb3fbb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Agendamento")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Agendamento")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
