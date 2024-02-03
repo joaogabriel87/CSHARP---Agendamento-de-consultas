6ad30b1193cc30ccfb7e0607482c3dd8acb32eb1
joaogabriel87 <joagabriel9@gmail.com>
2024-02-02 23:32:38 -0300

    commit all

--- Models/User.cs
+++ Models/User.cs
@@ -2,8 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using Newtonsoft.Json.Converters;
 
 namespace Agendamento.Models
 {
@@ -30,6 +33,7 @@ namespace Agendamento.Models
         [Required]
         public string Senha { get; set; }
         [Required]
+        [JsonConverter(typeof(StringEnumConverter))]
         public UserRole Role { get; set; }
 
         // Relacionamentos
